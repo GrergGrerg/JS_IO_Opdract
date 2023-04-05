bce87faf69f9485a280586de60719dcfc2a47233
Joachim <r0947348@student.thomasmore.be>
2023-04-05 18:27:50 +0200

    script + declaratie toegevoegd

---
+++ formscript.js
@@ -0,0 +1,22 @@
+// Declaratie
+const button = document.querySelector("#button");
+
+const voornaam = document.querySelector("#voornaam");
+const naam = document.querySelector("#naam");
+const gebruiker = document.querySelector("#gebruikersnaam");
+const email = document.querySelector("#email");
+const wachtwoord1 = document.querySelector("#wachtwoord1");
+const wachtwoord2 = document.querySelector("#wachtwoord2");
+const adres = document.querySelector("#adres");
+const land = document.querySelector("#land");
+const provincie = document.querySelector("#provincie");
+const postcode = document.querySelector("#postcode");
+const nieuwsbrief = document.querySelector("#nieuwsbrief");
+const voorwaarden = document.querySelector("#voorwaarden");
+
+const error = document.querySelector("#rood");
+const gelukt = document.querySelector("#groen");
+const betaling = document.querySelector("#blauw");
+
+
+button.addEventListener()
